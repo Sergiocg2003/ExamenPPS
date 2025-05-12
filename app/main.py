b1c388a22ccfd1bfcc71c58b56e19460be551195
sergio <scorgon265@g.educaand.es>
2025-05-12 17:46:11 +0200

    Corregir por flake

--- app/main.py
+++ app/main.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI
 from app.routers import tasks
 from app.database import create_db_and_tables
 
+
 app = FastAPI()
+
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
-app.include_router( tasks.router )
+
+app.include_router(tasks.router)
+
 
 @app.get("/version")
 def version():
     return {"message": "Corrales Gonzalez, Sergio - v0"}
-
